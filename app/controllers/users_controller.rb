8edc006b6e20e3c712d0c68621cb6d9598812aa8
bye-bye-kin <21431061ms@tama.ac.jp>
2020-10-16 20:56:46 +0900

    fix/profile_image.size

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
   def edit
     @user = User.find(params[:id])
     if @user != current_user
