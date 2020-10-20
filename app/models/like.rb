7e2d97946fc7f417b050194a215d3c6bfedb0dd5
bye-bye-kin <21431061ms@tama.ac.jp>
2020-10-20 22:46:19 +0900

    add/Like_routes,controller

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :nails
+
+  validates :user_id, uniqueness: {scope: :post_id}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "nails#index"
   resources :users
-  resources :nails
+  resources :nails do
+    resources :likes, only: %i(create destroy)
+  end
 end
